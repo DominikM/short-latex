f9afc051eced72127ca27d595b4d977a628de67a
Dominik Martinez <dominikm@mit.edu>
2016-12-24 20:55:17 -0800

    cont

--- static/js/latex_producer.js
+++ static/js/latex_producer.js
@@ -8,5 +8,34 @@ function update_latex() {
     latex = latex_box.value;
     MathJax.Hub.Queue(["Text", latex_display, latex]);
 }
+
+function save_latex() {
+    latex_text = latex_box.value;
+    
+    $.post(
+	page_data.save_url,
+	{latex_text: latex_text},
+	function (data) {
+	    if (data.result == 'success') {
+		var url_cont = document.getElementById('latex_url_container');
+		
+		var url_p = document.createElement('p');
+		var url_input = document.createElement('input');
+
+		url_p.innerHTML = 'The url for this latex is: ';
+
+		url_input.value = data.latex_url;
+		url_input.readOnly = true;
+
+		url_cont.appendChild(url_p);
+		url_cont.appendChild(url_input);
+	    }
+	}
+    );
+}
+
+
+		
+	
     
     
